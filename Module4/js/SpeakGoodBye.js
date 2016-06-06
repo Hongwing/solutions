ea40dac43cd3525a478f2f8eff4342afed9e41fe
Hongwing <1045243700@qq.com>
2016-06-06 22:36:30 +0800

    solve the Module4

---
+++ Module4/js/SpeakGoodBye.js
@@ -0,0 +1,14 @@
+/**
+* IIFE
+*/
+(function speak (window,name) {
+	var byeSpeaker = {};
+
+	var speakWord = "Good Bye ";
+	byeSpeaker.speak = function (name) {
+		console.log(speakWord + name);
+}
+
+	window.byeSpeaker = byeSpeaker;
+
+})(window,name);
---
+++ Module4/js/SpeakHello.js
@@ -0,0 +1,13 @@
+/**
+* IIFE
+*/
+(function speak (window,name) {
+	var helloSpeaker = {};
+
+	var speakWord = "Hello ";
+	helloSpeaker.speak = function (name) {
+	console.log(speakWord + name);
+}
+
+	window.helloSpeaker = helloSpeaker;
+})(window,name);
